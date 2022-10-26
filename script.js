a58c512e8fdad8ff94ae5577ce58eea65ec44d23
IsabelErlandsson <isabelle.erlandssons@gmail.com>
2022-10-26 18:10:51 +0200

    Added result button and info, some style

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // DARKMODE
-// QuerySelector och ändra scr. 
 let darkMode = document.querySelector("#darkmode-toggle");
 let image = document.querySelector("#Blacktxtimg");
 
@@ -9,89 +8,91 @@ if(darkMode) {
         document.body.style.background = "#292929";
         document.body.style.color = "#F4F4F4";
         image.src = "img/Whitetxt.png";
+        document.body.style.borderColor = "white";
     } else{
         document.body.style.background = "#FFFFFF";
         document.body.style.color = "#3a3a3a";
         image.src = "img/Blacktxt.png";
+        document.body.style.borderColor = "black";
 
     }
   }
 )}
-//Samtliga frågor/alternativ/svar_________________________________
+
+//My array of questions and correctAnswers______________________________________
 let questionsArray = [
-    {
-     question: "Hur många program finns det på Ankademin?",
-     choice:["Tre","Sex","Nio","Tolv"],
-     answer:"Nio"
+{
+     question: "Vilken sångerskas riktiga namn är Stefani Joanne Angelina Germanotta?",
+     choice:["Sia","Lady Gaga","Kesha","Beyoncé"],
+     correctAnswer:"Lady Gaga"
      },
     {
-     question: "Ankademin ligger i Barcelona.",
-     choice: ["True", "False"],
-      answer: "False"
+     question: "Succéserien Squid Game kommer från Sydkorea.",
+     choice: ["Sant", "Falskt"],
+      correctAnswer: "Sant"
     },
     {
-     question: "På Ankademin kan man läsa till Frontend Developer.",
-     choice: ["True", "False"],
-      answer: "True"
+     question: "Madonna's real name is Magdalena",
+     choice: ["Sant", "Falskt"],
+      correctAnswer: "Falskt" // Correct correctAnswer is Madonna. 
     },
     {
-     question: "Vilken/vilka kända personer har gått på Ankademin?",
-     choice: [ "1","2","3","4"],
-      answer: ["2","3"]
+     question: "Vilken/vilka filmer har vunnit 11 Oscars",
+     choice: [ "Sista sommaren","The Informer","Ben Hur", " Titanic"],
+      correctAnswer: ["Ben Hur", " Titanic"]
     },
     {
-     question: "På Ankademin börjar lektionerna alltid 07:00.",
-     choice: ["True", "False"],
-      answer: "False"
+     question: "Meryl Streep har vunnit två Academy Awards.",
+     choice: ["Sant", "Falskt"],
+      correctAnswer: "Falskt" // Correct correctAnswer is three. 
     },
     {
-     question: "Ankademin erbjuder fem olika program att studera.",
-     choice: ["True", "False"],
-      answer: "True"
+     question: "Vad heter Batmans butler?",
+     choice: ["Henry","Robin", "Alfred", "Charles"],
+      correctAnswer: "Alfred"
     },
     {
-     question: "Varje fredag erbjuder Ankademin glass till sina elever.",
-     choice: ["True", "False"],
-      answer: "True"
+     question: "Vem eller vilka har spelat huvudkaraktären James Bond?",
+     choice: ["Daniel Craig", " Pierce Brosnan", "Brad Pitt", " Sean Connery"],
+      correctAnswer: ["Daniel Craig", " Pierce Brosnan", " Sean Connery"]
     },
     {
-     question: "Praktik är inget som Ankademin erbjuder i sina utbildningar.",
-     choice: ["True", "False"],
-      answer: "False"
+     question: "Hur många avsnitt finns det av serien Vänner?",
+     choice: ["216 episodes", "226 episodes", "236 episodes", " 246 episodes"],
+       correctAnswer: "236 episodes"
     },
     {
-     question: "Ankademin har funnits sedan 1925.",
-     choice: ["True", "False"],
-      answer: "False"
+     question: "Harry Styles mellannamn är Edward.",
+     choice: ["Sant", "Falskt"],
+      correctAnswer: "Sant"
     },
     {
-     question: "Ankademin erbjuder alla elever laptops.",
-     choice: ["True", "False"],
-     answer: "False"
+     question: "I Harry Potter har Draco Malfoy en lillasyster.",
+     choice: ["Sant", "Falskt"],
+     correctAnswer: "Sant"
     },
   ];
  
-// Frågor_________________________________________________________
-// let quizQuestions = document.getElementById("questions");
 let p = document.querySelector("#score");
 let totalScore = 0;
 let maxPoints = 10;
 
-
+// Get questions_________________________________________________________
 questionsArray.forEach((obj,i) => {
-  let p = document.createElement("p");
+  let p = document.createElement("h2");
   p.innerText = obj.question;
   document.body.append(p);
 
-// Svarsalternativ och radio buttons/checkbox______________________________
-  obj.choice.forEach((choice) => {
-    let p = document.createElement("p");
-    p.innerText = choice 
-    document.body.append(p); 
-    let radioBtn = document.createElement("input");
+// Get choices, radio buttons, checkboxes________________________________
+obj.choice.forEach((choice) => {
+  let p = document.createElement("p");
+  p.innerText = choice 
+  document.body.append(p); 
 
-  // Om answer inte har en array har alternativen en radiobutton. Om answer har en array så har alternativen en checkbox. 
-    if (!Array.isArray(obj.answer) ) {
+let radioBtn = document.createElement("input");
+// If correctAnswer is not an array --> radio button__________________________ 
+// if correctAnswer is an array ---> checkbox_________________________________
+    if (!Array.isArray(obj.correctAnswer) ) {
       radioBtn.type="radio";
     } else {
       radioBtn.type = "checkbox";
@@ -99,10 +100,9 @@ questionsArray.forEach((obj,i) => {
     radioBtn.value = choice 
     p.append(radioBtn);
 
-    radioBtn.name = `selectOneAnswer-${i}`;
+    radioBtn.name = `selectOnecorrectAnswer-${i}`;
   })
 }); 
-
 //________________________________________________________________
 
 let submitBtn = document.getElementById("submitBtn");
@@ -110,43 +110,63 @@ let submitBtn = document.getElementById("submitBtn");
 submitBtn.addEventListener("click", () => {
   questionsArray.forEach((question, i) => {
     
-    // if question === radio --> we count like this. 
-    if(!Array.isArray(question.answer)) {
-      let userAnswer = document.querySelector(`[name='selectOneAnswer-${i}']:checked`).value
-      if (userAnswer === question.answer) {
-        totalScore++;
+    // if question === radio --> we count like this_______________
+    if(!Array.isArray(question.correctAnswer)) {
+      let usercorrectAnswer = document.querySelector(`[name='selectOnecorrectAnswer-${i}']:checked`).value
+      if (usercorrectAnswer === question.correctAnswer) {
+        totalScore++; 
       } else {
       }
+// else question === checkbox --> we count like this__________
+} else {  
+  let isCheckBox = document.querySelectorAll(`[name='selectOnecorrectAnswer-${i}']:checked`);
+  let checkboxList = [];
+  isCheckBox.forEach((box) => {
+    checkboxList.push(box.value);
+  });
 
-    // else question === checkbox --> we count like this.
-    } else {  
-    let isCheckBox = document.querySelectorAll(`[name='selectOneAnswer-${i}']:checked`);
-    let checkboxList = [];
-    isCheckBox.forEach((box) => {
-      checkboxList.push(box.value);
-    });
-
-    let correctUserAnswers = checkboxList.filter(x => {
-      return question.answer.includes(x)
-    })
-
-    if (correctUserAnswers.length === question.answer.length && checkboxList.length === question.answer.length ) {
-        totalScore++;
-    }
-    }
+  let correctUsercorrectAnswers = checkboxList.filter(x => {
+    return question.correctAnswer.includes(x)
   })
-  
-    // RESULT
-  if(totalScore > maxPoints * 0.75) {
-    p.style.color = "green";
-    p.innerText = `${totalScore}/10. Mycket väl godkänd!`; 
-} else if (totalScore > maxPoints * 0.5) {
-    p.style.color = "orange";
-    p.innerText = `${totalScore}/10. Godkänt!`;
-} else {
-    p.style.color = "red";
-    p.innerText = `${totalScore}/10. Underkänt!`;
-}
-return totalScore;
-})
 
+  if (correctUsercorrectAnswers.length === question.correctAnswer.length && checkboxList.length === question.correctAnswer.length ) {
+      totalScore++;
+  } else {
+  }
+  }
+
+// RESULT______________________________________________________
+     if(totalScore > maxPoints * 0.75) {
+      p.style.color = "#1C8B0A";
+      p.innerText = `${totalScore}/10. Mycket väl godkänd!`; 
+  } else if (totalScore > maxPoints * 0.5) {
+      p.style.color = "#BE6716";
+      p.innerText = `${totalScore}/10. Godkänt!`;
+  } else {
+      p.style.color = "#B71B1B";
+      p.innerText = `${totalScore}/10. Underkänt!`;
+  }
+  return totalScore;
+  
+  });
+
+})
+  
+// See correct answwers for each question___________________________
+let showAnswers = document.querySelector("#answers");
+let showCorrectAnswerBtn = document.createElement("button");
+
+showCorrectAnswerBtn.innerText = "Visa rätta svaren";
+document.body.append(showCorrectAnswerBtn, showAnswers);
+
+showCorrectAnswerBtn.addEventListener("click", () => {
+  if (showAnswers.innerHTML === "") {
+    questionsArray.forEach((Q) => {
+      showAnswers.innerHTML += 
+    `<div>
+      <h4>Fråga: ${Q.question}</h4>
+      <p id="correctAnswer" >Svar: ${Q.correctAnswer}</p>
+    </div>`;
+    });
+  } 
+});
